d53e1555feb46071400dc9966953691c6282ba3f
Netzer <netzer.epstein@mail.huji.ac.il>
2022-01-05 16:26:05 +0200

    player adjustments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,51 +5,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform mPlayer;
-    [SerializeField] private float mMoveSpeed = 1;
     [SerializeField] private GameManager gameManager;
 
+    public Rigidbody2D rb;
 
-    void Update()
+    private void Awake()
     {
-        // TODO: stop the player movement if it's low
-        
-        // TODO: do we want to clear the arrows? well, yes.
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            mPlayer.Translate(Vector2.up * mMoveSpeed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            mPlayer.Translate(Vector2.down * mMoveSpeed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            mPlayer.Translate(Vector2.right * mMoveSpeed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            mPlayer.Translate(Vector2.left * mMoveSpeed * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log($"player Triger with {other.tag}");
-        if (other.tag == "Goal")
-        {
-            Debug.Log("Win");
-            gameManager.SetScreen();
-        }
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log($"player Collide with {other}");
-        if (other.gameObject.tag == "Goal")
+        if (other.gameObject.CompareTag("Goal"))
         {
             Debug.Log("Win");
             gameManager.SetScreen();
